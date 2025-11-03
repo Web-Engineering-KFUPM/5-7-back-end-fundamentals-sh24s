f2dfb674de9352b89b87c1ffe394d09a46a37221
sh24s <144224701+sh24s@users.noreply.github.com>
2025-11-03 11:55:11 +0300

    DONE!

--- 5-7-back-end-fundamentals/src/App.jsx
+++ 5-7-back-end-fundamentals/src/App.jsx
@@ -22,8 +22,8 @@ LAB SETUP INSTRUCTIONS
 
 4. Start the back-end server from a separate terminal, path: 5-7-back-end-fundamentals-Dromarjh-main\5-7-back-end-fundamentals\backend:
    Run:
-      node server.js
 
+   
   If your system blocks running npm commands (especially on Windows PowerShell),
    run this command first to allow script execution:
       Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
